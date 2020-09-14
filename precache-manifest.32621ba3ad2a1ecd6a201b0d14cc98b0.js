fa0414583ea3de2dcc02286331d8686b56a16720
Ira <irarad1994@gmail.com>
2020-09-14 22:39:43 +0300

    Updates

---
+++ precache-manifest.32621ba3ad2a1ecd6a201b0d14cc98b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491949e89afb28919e7fda27234e88d4",
+    "url": "/HW17-advanced/index.html"
+  },
+  {
+    "revision": "3247a25f90c6f8756b64",
+    "url": "/HW17-advanced/static/css/main.cead968b.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/HW17-advanced/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW17-advanced/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3247a25f90c6f8756b64",
+    "url": "/HW17-advanced/static/js/main.a8b41ae7.chunk.js"
+  },
+  {
+    "revision": "9bb7b20e28afa11e5c66",
+    "url": "/HW17-advanced/static/js/runtime-main.4fc62025.js"
+  }
+]);
